beefcf469cd5b2fbc8dcd11b1c7e4cd997b11545
Olexander Krasulia <krasulazr@gmail.com>
2023-10-10 17:45:11 +0300

    fix deploy 3

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -27,9 +27,9 @@ function Skill({ directionLeft, images, progress, index }: SkillProps) {
           }}
           src={images}
           alt={`Skill`}
-          className='rounded-full border border-gray-500 object-cover w-14 md:w-20 xl:w-32 h-14 md:h-20 xl:h-32 p-2 md:p-3 filter group-hover:grayscale transition duration-300 ease-in-out'
+          className='rounded-full border border-gray-500 object-cover w-22 md:w-20 xl:w-32 h-14 md:h-20 xl:h-32 p-2 md:p-3 filter group-hover:grayscale transition duration-300 ease-in-out'
         />
-        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-14 md:h-[75px] xl:h-32 w-14 md:w-[75px] xl:w-32 rounded-full z-0'>
+        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-14 md:h-[75px] xl:h-32 w-20 md:w-[75px] xl:w-32 rounded-full z-0'>
         <div className='flex items-center justify-center h-full'>
           <p className='text-lg md:text-3xl font-bold text-black opacity-100'>{progress}</p>
         </div>
